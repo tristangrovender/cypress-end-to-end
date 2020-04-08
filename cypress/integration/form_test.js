59d5a827c502e8a69f6954cfa1c65726fa8ac0c8
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-08 10:44:39 -0500

    tests written for form fields

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,5 +1,20 @@
 describe("Testing our volunteer from", function() {
+    beforeEach(function() {
+        cy.visit("http://localhost:3000/"); // test for being able to access page
+    });
     it("Add test to inputs and submit form", function() {
-        cy.visit("http://localhost:3000/");
+        cy.get('[for="name"] > input')
+            .type("Tristan")
+            .should("have.value", "Tristan");
+        cy.get("input[name='email']")
+            .type("email@email.com")
+            .should("have.value", "email@email.com");
+        cy.get("textarea")
+            .type("I have a HUGE heart.")
+            .should("have.value", "I have a HUGE heart.");
+        cy.get("#positions")
+            .select("Yard Work")
+            .should("have.value", "Yard Work");
+        cy.get('[type="checkbox"]').check();
     });
 });
